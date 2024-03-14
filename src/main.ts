dc8b87301ed7774ca3be46e25a0f65e0467d414e
bastien girschig <bgirschig@google.com>
2024-03-14 13:27:16 +0100

    load all cards by default

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,19 @@ import * as testCards from './testCard';
 
 function main() {
   const params = loadUrlParams();
-  testCards.loadTestCards(params.modules);
+  const availableModules = testCards.listAvailableModules();
+
+  if (params.modules.length === 0) testCards.loadTestCards(availableModules);
+  else testCards.loadTestCards(params.modules);
+  
   testCards.config.confettiOnSuccess = !params.noConfetti;
-  console.log(testCards.listAvailableModules());
 }
 
 function loadUrlParams() {
   const searchParams = new URLSearchParams(window.location.search);
   const modules = searchParams.get("modules") || "";
   return {
-    modules: modules.split(",").map(item=>item.trim()),
+    modules: modules.split(",").map(item=>item.trim()).filter(Boolean),
     noConfetti: searchParams.has("no-confetti"),
   }
 }
